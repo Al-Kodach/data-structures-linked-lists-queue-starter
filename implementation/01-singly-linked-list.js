0c673eaf75e88d61e7cb2c1d69518c6f69c76e23
Al-kodach <alkodach351@gmail.com>
2023-05-18 18:50:12 -0400

    prog

--- implementation/01-singly-linked-list.js
+++ implementation/01-singly-linked-list.js
@@ -12,55 +12,110 @@ class SinglyLinkedList {
         this.length = 0;
     }
 
-    addToHead(val) { 
+    addToHead(val) {
         // Add node of val to head of linked list
+        let node = new SinglyLinkedList(val);
+
+        // add to head if head is empty and return
+        if (!this.head) {
+            this.head = node;
+            this.length++;
+            return;
+        }
+
+        // link new node to head and assign new node to head
+        node.next = this.head;
+        this.head = node;
+        this.length++;
 
         // Write your hypothesis on the time complexity of this method here
+        /*
+            visitiong each method in is O(1) constant
+            adding value to head is O(1), does not reqiure to visit any node
+        */
     }
 
     addToTail(val) {
         // There are bugs in this method! Fix them!!!
         // Write your hypothesis on the time complexity of this method here
+        /*
+            time complexity is O(n) since we have to visit each node to find the last node
+        */
 
         // Add node of val to tail of linked list
-        let newNode = new SinglyLinkedNode(data);
+        let newNode = new SinglyLinkedNode(val);
 
-        if (!head) {
-            head = newNode;
-            return head;
+        // if head is empty we set to head and return
+        if (!this.head) {
+            this.head = newNode;
+            this.length++;
+            return;
         }
 
-        let curr = head;
+        // traverse each node to find the first node which is the last
+        let curr = this.head;
         while (curr) {
-            curr = current.next;
-        }
-        curr.next = newNode;
 
-        return head;
+            if (!curr.next) {
+                curr.next = newNode;
+                this.length++
+                return;
+            }
+            curr = curr.next;
+        }
     }
 
     removeFromHead() {
         // Remove node at head
+        if (this.head) {
+            this.head = this.head.next;
+            this.length--;
+            return;
+        }
 
         // Write your hypothesis on the time complexity of this method here
+        // time complexity is O(1);
     }
 
     removeFromTail() {
         // Remove node at tail
+        if (!this.head) return null;
+
+        // tranverse each node to find the last
+        let cur = this.head;
+        while (cur) {
+            // if curr is not linked, that is the last node
+            if (!cur.next.next) {
+                cur.next = null;
+                this.length--;
+                return;
+            }
+            cur = cur.next;
+        }
 
         // Write your hypothesis on the time complexity of this method here
+        // visiting each node in the link is O(n), time complexity in total is O(n)
     }
 
     peekAtHead() {
         // Return value of head node
-
+        return this.head.val || null;
         // Write your hypothesis on the time complexity of this method here
+        // visited only the first node, hence tiem complexity is O(1);
     }
 
     print() {
         // Print out the linked list
-        
+        let cur = this.head;
+
+        while (cur) {
+            console.stdout.write(`${cur.val} -> `);
+            cur = cur.next;
+        }
+
+        console.log('Null');
         // Write your hypothesis on the time complexity of this method here
+        // visited every every node in the list is O(1), hence tiem complexity is O(1);
     }
 }
 
--- implementation/02-doubly-linked-list.js
+++ implementation/02-doubly-linked-list.js
@@ -21,7 +21,7 @@ class DoublyLinkedList {
         // Add node of val to head of linked list
         let newNode = new DoublyLinkedNode(val);
 
-        if (this.length >= 0) {
+        if (this.head) {
             this.head.previous = newNode;
             newNode.next = this.head;
             this.head = newNode;
@@ -31,35 +31,57 @@ class DoublyLinkedList {
         }
 
         this.length++;
+
+        // time coplexity is O(1)
     }
 
     addToTail(val) {
         // Add node of val to tail of linked list
+        let newNode = new DoublyLinkedNode(val);
 
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.length++;
+            return;
+        }
+
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
         // Write your hypothesis on the time complexity of this method here
+        // time coplexity is O(1), since we only
+        // 1, visit the tail which is O(1) 2, visit prev which is also O(1)
     }
 
     removeFromHead() {
         // Remove node at head
-
+        thie.head = this.head.next || this.tail;
         // Write your hypothesis on the time complexity of this method here
+        // time complexity is O(1); we only replace head linked node to head and
+        // the memory will get ride of the space occapied by head
     }
 
     removeFromTail() {
         // Remove node at tail
-
+        this.prev.next = null;
+        this.prev = this.prev.prev || null;
+        this.tail = this.prev || null;
         // Write your hypothesis on the time complexity of this method here
+        // time coplexity is O(1), since we only have
+
     }
 
     peekAtHead() {
         // Return value of head node
-
+        return this.head.value || null;
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtTail() {
         // Return value of tail node
-        
+        return this.tail.val || null;
         // Write your hypothesis on the time complexity of this method here
     }
 }
